6c53c63d67fd62396a704be8cfa838f557890371
negibokken <negibokken@gmail.com>
2020-09-20 02:53:15 +0900

    Add sitemap and sitemap generator

--- blog.bokken.io/scripts/blog-index-gen.js
+++ blog.bokken.io/scripts/blog-index-gen.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const ejs = require('ejs');
 const bm2h = require('./bmarkdown2html');
-const { BMarkdown2HTML } = require('./bmarkdown2html');
+const {BMarkdown2HTML} = require('./bmarkdown2html');
+const {format} = require('prettier');
 
 const mdFiles = [];
-function scanFiles(path, ext) {
+function scanFiles(path, ext, ignore = '') {
   const files = fs.readdirSync(path);
   for (const file of files) {
+    if (file == ignore) continue;
     const curPath = `${path}/${file}`;
     if (fs.lstatSync(curPath).isDirectory()) {
       scanFiles(curPath, ext);
@@ -18,12 +20,12 @@ function scanFiles(path, ext) {
 
 (async () => {
   try {
-    scanFiles('articles', '.md');
+    scanFiles('articles', '.md', 'current');
     let articles = [];
     for (const mdFile of mdFiles) {
       const data = fs.readFileSync(mdFile).toString();
       const url = mdFile.replace('.md', '.html');
-      const m = new BMarkdown2HTML(data, null, { url });
+      const m = new BMarkdown2HTML(data, null, {url});
       const article = {
         filepath: url,
         date: m.dates[0],
@@ -35,8 +37,9 @@ function scanFiles(path, ext) {
 
     articles = articles.sort((a, b) => a > b);
     const template = fs.readFileSync('./templates/blog-index.ejs').toString();
-    const html = ejs.render(template, { articles });
-    console.log(html);
+    const html = ejs.render(template, {articles});
+    // console.log(html);
+    console.log(format(html, {parser: 'html'}));
     process.exit(0);
   } catch (e) {
     console.error(e);
