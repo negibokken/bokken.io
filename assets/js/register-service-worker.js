6e810f61502e656e0b79042d8824e894a27c0794
negibokken <negibokken@gmail.com>
2020-10-25 16:19:39 +0900

    Add manifest and service worker

---
+++ assets/js/register-service-worker.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then((registration) => {
+        console.log(
+          'Successfully registered service worker with scope: ',
+          registration.scope
+        );
+      })
+      .catch((err) => {
+        console.error('Failed to register service worker with err: ', err);
+      });
+  });
+}
