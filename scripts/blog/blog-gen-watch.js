dd68c3465169f385cc508648e7f12b3ed7a8f790
Ryo Ogawa <ryo.ogawa@nex.nikkei.com>
2023-09-29 20:36:49 +0900

    update(script): Update script output

--- scripts/blog/blog-gen-watch.js
+++ scripts/blog/blog-gen-watch.js
@@ -1,23 +1,30 @@
 const watch = require('node-watch');
-const { exec } = require('node:child_process')
+const { exec } = require('node:child_process');
 const fs = require('fs');
 
-const articlesPath = "sites/blog.bokken.io/articles"
+const articlesPath = 'sites/blog.bokken.io/articles';
 
-watch(articlesPath, { recursive: true, filter: /\.md/ }, function(evt, name) {
-    exec(`yarn --silent build:blog:article:dev ${name} ${name} ${name}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(error);
-            return;
+watch(articlesPath, { recursive: true, filter: /\.md/ }, function (evt, name) {
+    exec(
+        `yarn --silent build:blog:article:dev ${name} ${name} ${name}`,
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            if (stdout) {
+                fs.writeFileSync(name.replace('.md', '.html'), stdout);
+                console.log(
+                    `evt: ${evt}, name: ${name
+                        .replace('.md', '.html')
+                        .replace('sites/', 'https://')}`
+                );
+                return;
+            }
+            if (stderr) {
+                console.error(stderr);
+                return;
+            }
         }
-        if (stdout) {
-            fs.writeFileSync(name.replace('.md', '.html'), stdout);
-            console.log(`evt: ${evt}, name: ${name}`);
-            return;
-        }
-        if (stderr) {
-            console.error(stderr)
-            return;
-        }
-    });
+    );
 });
