4d4cca5b361246071d1954150d515e2e38b6bd19
bokken <negibokken@gmail.com>
2025-02-16 16:53:13 +0900

    WIP: Replace with Astro

---
+++ astro/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  experimental: {
+    clientPrerender: true,
+  },
+});
