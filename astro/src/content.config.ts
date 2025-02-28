847ba12dc126fc99e6bb7b0debee221668947244
bokken <negibokken@gmail.com>
2025-03-01 06:25:18 +0900

    wip

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -2,14 +2,14 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/articles/` directory.
-  loader: glob({ base: './src/content/articles', pattern: '**/*.{md,mdx}' }),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    pubDate: z.string(),
-    updatedDate: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
 });
