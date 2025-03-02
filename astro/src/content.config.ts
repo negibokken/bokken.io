001080af72731ed692f87a12720ce384c14b96ba
bokken <negibokken@gmail.com>
2025-03-02 22:42:08 +0900

    wip

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
+    tags: z.array(z.string()),
   }),
 });
 
