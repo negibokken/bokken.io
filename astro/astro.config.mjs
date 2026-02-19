57a22434353609fd7d60182e2ad03e92b2f7f942
Ryo Ogawa <negibokken@gmail.com>
2026-02-19 13:31:35 +0900

    feat(astro): set outDir to dist/blog.bokken.io in astro config

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.bokken.io",
+  outDir: "dist/blog.bokken.io",
   integrations: [mdx(), sitemap()],
   prefetch: {
     prefetchAll: true,
