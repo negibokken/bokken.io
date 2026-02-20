becc4dca24701b6007e508d269083a19768e913a
Ryo Ogawa <negibokken@gmail.com>
2026-02-20 11:30:37 +0900

    refactor: Remove old blog generation scripts

--- scripts/blog/lib/getRelativePath.js
+++
@@ -1,7 +0,0 @@
-function getRelativePath(path, ext) {
-    const basePath = `${ext ? './' : ''}sites/blog.bokken.io/`;
-    const replacedPath = path?.replace(basePath, "")
-    return replacedPath;
-}
-
-module.exports = { getRelativePath };
