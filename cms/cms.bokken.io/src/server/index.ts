ab7cb0a211f460aad3619a003fbdfec4e70f142e
bokken <negibokken@gmail.com>
2026-03-18 09:04:28 +0900

    chore: remove cms/ directory

--- cms/cms.bokken.io/src/server/index.ts
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-import { config } from "./config.js";
-
-app.listen(config.port, "0.0.0.0", () => {
-  console.log(`CMS server running on http://0.0.0.0:${config.port}`);
-});
